fb95e84650a1415475470972e5e353afed8a73eb
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-22 00:41:20 +0530

    chore: Add react-icons npm dependency and update AboutSection component

--- src/components/AboutPage/AboutSection.jsx
+++ src/components/AboutPage/AboutSection.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { FaArrowRightLong } from 'react-icons/fa6'
 
 const AboutSection = () => {
   return (
-    <section className='p-[30px] my-[50px]'>
-        <h1></h1>
+    <section>
+        <div className='flex justify-between items-center border-y text-white hover:text-black hover:bg-white border-y-white px-[30px] py-[50px] transition-all duration-[500ms] ease-in-out'>
+            <p className='group text-[60px]  font-medium ' style={{fontFamily: 'Playfair Display'}}>
+                Start reading
+            </p>
+            <FaArrowRightLong className='text-5xl'/>
+        </div>
+        <div className='flex justify-between items-center border-y text-white hover:text-black hover:bg-white border-y-white px-[30px] py-[50px] transition-all duration-[500ms] ease-in-out'>
+            <p className='group text-[60px]  font-medium ' style={{fontFamily: 'Playfair Display'}}>
+                Start writing
+            </p>
+            <FaArrowRightLong className='text-white text-5xl'/>
+        </div>
+        <div className='flex justify-between items-center border-y text-white hover:text-black hover:bg-white border-y-white px-[30px] py-[50px] transition-all duration-[500ms] ease-in-out'>
+            <p className='group text-[60px]  font-medium ' style={{fontFamily: 'Playfair Display'}}>
+                Become a member
+            </p>
+            <FaArrowRightLong className='text-white text-5xl'/>
+        </div>
     </section>
   )
 }
