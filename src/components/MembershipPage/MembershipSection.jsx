b82e42551266116d5b6013a736e3d69862f26b86
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-22 18:03:16 +0530

    chore: Update MembershipSection component with react-icons and new styling

--- src/components/MembershipPage/MembershipSection.jsx
+++ src/components/MembershipPage/MembershipSection.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { TiTick } from 'react-icons/ti'
+import { FaStar, FaHeart } from 'react-icons/fa'
 
 const MembershipSection = () => {
 
@@ -10,7 +12,7 @@ const MembershipSection = () => {
         document.getElementById("colourChange").style.transition = 'all 3s ease-in-out'
     }, 3000);
   return (
-    <section>
+    <section style={{fontFamily: 'Playfair Display'}}>
         <div className='flex mt-[85px] w-full border-b border-black'>
             <div className='text-lg tracking-wide  pr-5 mr-5 flex flex-col w-1/3 px-[30px] leading-8' style={{fontFamily: 'Playfair Display'}}>
                 <h1 className='text-[60px] sticky top-[200px] leading-tight  mb-[50px]'>
@@ -61,17 +63,69 @@ const MembershipSection = () => {
             </div>
         </div>
         <div className='flex mt-[85px] w-full border-b border-black'>
-            <div className='text-lg h-[90vh] tracking-wide  pr-5 mr-5 flex flex-col w-1/3 px-[30px] leading-8' style={{fontFamily: 'Playfair Display'}}>
+            <div className='text-lg h-[90vh] tracking-wide  pr-5 mr-5 flex flex-col w-1/3 px-[30px] leading-8'>
                 <h1 className='text-[60px] sticky top-[200px] leading-tight  mb-[50px]'>
                     Membership plans
                 </h1>
             </div>
             <div className='w-2/3 h-[90vh] flex gap-5 px-5 pl-5 mx-5 ml-auto'>
-                <div className='w-1/3 h-[80vh] rounded border m-1'>
-
+                <div className='w-1/3 h-[80vh] rounded border m-1 flex flex-col justify-center items-center p-5' style={{fontFamily: 'Poppins'}}>
+                    <div className='flex justify-center items-center m-3 p-3'>
+                    <FaStar className='text-yellow-400 text-3xl'/>
+                    </div>
+                    <div className='w-full text-center' >
+                        <h1 className='text-2xl font-medium tracking-tighter' >
+                        Vichaar Member
+                        </h1>
+                        <p className='text-sm'>
+                        $5/month or $60/year
+                        </p>
+                    </div>
+                    <button
+                    className='w-full bg-green-600 text-white text-sm py-2 px-4 mt-5 rounded-full transition-all duration-100 ease-in-out hover:drop-shadow-md'
+                    >Get Started</button>
+                    <div>
+                        <ul className='text-sm flex flex-col gap-5 mt-[40px]'>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Read member-only stories</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Support writers you read most</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Earn money for your writing</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Listen to audio narrations</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Read offline with the Medium app</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Access our Mastodon community</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Connect your custom domain</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin'/>Create your own publications</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className='w-1/3 h-[80vh] rounded border m-1'>
-
+                <div className='w-1/3 h-[80vh] rounded border m-1 flex flex-col p-5' style={{fontFamily: 'Poppins'}}>
+                    <div className='flex justify-center items-center m-3 p-3'>
+                    <FaHeart className='text-yellow-400 text-3xl'/>
+                    </div>
+                    <div className='w-full text-center' >
+                        <h1 className='text-2xl font-medium tracking-tighter' >
+                        Vichaar Family
+                        </h1>
+                        <p className='text-sm'>
+                        $15/month or $150/year
+                        </p>
+                    </div>
+                    <button
+                    className='w-full bg-green-600 text-white text-sm py-2 px-4 mt-5 rounded-full transition-all duration-100 ease-in-out hover:drop-shadow-md'
+                    >Get Started</button>
+                    <div>
+                        <p className='my-5 flex mt-[40px]'>
+                            <FaStar className='text-yellow-400 text-sm'/>
+                            <span className='text-sm px-3'>All Vichaar member benefits</span>
+                        </p>
+                        <p className='font-normal text-sm text-center m-5'>
+                            PLUS
+                        </p>
+                        <ul className='text-sm flex flex-col gap-5 '>
+                        <li className='flex items-center gap-3 font-semibold'><TiTick className='text-lg text-green-600 font-thin'/>Writers earn 4x when you read their stories</li>
+                        <li className='flex items-center gap-3'><TiTick className='text-green-600 font-thin text-3xl'/>Share member-only stories with anyone and drive more earnings for writers</li>
+                        
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
