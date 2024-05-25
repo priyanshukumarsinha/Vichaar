04e52df72e8cbc3999c5e031bfe35eb7e79e445c
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-26 01:37:56 +0530

    chore: Update HomePage with lazy loading for images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import HomeHeader from '../components/HomePage/HomeHeader';
 import HomeSection from '../components/HomePage/HomeSection';
-import AboutFooter from '../components/AboutPage/AboutFooter';
+import { FaStar } from 'react-icons/fa';
+import { FaX } from "react-icons/fa6";
 
 const HomePage = () => {
   const isAuthenticated = localStorage.getItem('user');
+  const [showPromotion, setShowPromotion] = useState(localStorage.getItem('showPromotion') || true);
   if(!isAuthenticated){
     console.log('User is not authenticated');
     return <Navigate to='/'/>
   };
   return (
-    <div className='flex flex-col justify-between h-screen'>
+    <div className='w-full h-full'>
       <HomeHeader />
-      <HomeSection />
-      <AboutFooter />
+      {
+        showPromotion == true && (
+          <div className='w-full bg-gray-50 p-3 flex gap-2 justify-center items-center relative'>
+        <FaStar className='text-yellow-400'/> 
+        <p className='text-xs font-medium'>
+        Get unlimited access to the best of Medium for less than $1/week.
+        {" "}<span className='font-semibold underline cursor-pointer'>Become a member</span>
+        </p>
+        <div className='text-black opacity-40 absolute right-10 '>
+          <FaX className='text-gray-500 cursor-pointer text-md'
+          onClick={() => {setShowPromotion(false); localStorage.setItem('showPromotion', false)}}
+          />
+        </div>
+      </div>
+        )
+      }
+      {/* <HomeSection /> */}
+      {/* <AboutFooter /> */}
     </div>
   )
 }
