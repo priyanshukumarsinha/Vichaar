72ab4cd8d937ac51282dc9b88649afa150849a9b
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2025-03-04 08:27:36 +0530

    Enhance blog functionality with comments and likes features

--- backend/src/router/blog.routes.ts
+++ backend/src/router/blog.routes.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { createBlog, getBlog, updateBlog } from "../controller/blog.controller";
+import { createBlog, getBlog, updateBlog, commentOnPost, deleteComment, getComments, likePost } from "../controller/blog.controller";
 
 type Bindings = {
   DATABASE_URL: string;
@@ -18,5 +18,14 @@ blog.post("/", createBlog);
 blog.put("/", updateBlog);
 
 blog.get(":id", getBlog);
+
+blog.post(":id/comment", commentOnPost);
+
+blog.get(":id/comments", getComments);
+
+blog.delete(":id/comment/:commentId", deleteComment);
+
+blog.post(":id/like", likePost);
+
 
 export { blog };
